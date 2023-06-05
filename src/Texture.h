b6fb03ce649e27c7bba2d28b8e531c76d4d6d911
Peter Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2023-06-05 10:46:27 +0100

    Add destructor for Texture

--- src/Texture.h
+++ src/Texture.h
@@ -3,9 +3,19 @@
 #include "SDL.h"
 
 struct Texture {
+    ~Texture() {
+        // Deallocate the texture data
+        // id, name, width, height, channels are automatically deallocated
+        if (sdlTexture != nullptr) {
+            SDL_DestroyTexture(sdlTexture);
+            sdlTexture = nullptr;
+        }
+    }
+
     short id;
     std::string name;
     SDL_Texture* sdlTexture;
     SDL_Renderer* sdlRenderer;
     int width, height, channels;
 };
+
